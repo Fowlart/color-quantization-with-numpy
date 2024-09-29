6b29b8199087fc2bdd75cef84fd97d46fb4f1848
artur.semikov <AG6CBZZ@mmm.com>
2024-09-29 19:15:02 +0300

    init commit

--- src/color-quantization-with-numpy/example.py
+++ src/color-quantization-with-numpy/example.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-import math as m
 import os
 
 
@@ -17,6 +16,7 @@ def quantize(image_array):
     height, width, _ = image_array.shape
 
     pixels = image_array.reshape(-1, 3)
+
     print(f"""
 pixels.shape: {pixels.shape}
     
@@ -28,21 +28,26 @@ pixels.mean: {pixels.mean()}
     
 pixels.max: {pixels.max()}
 
-""".strip())
+pixel example: {pixels[165234, :]}
+""")
 
     codebook = np.array([
         [0.0, 0.0, 0.0],  # Black
         [1.0, 1.0, 1.0],  # White
         [1.0, 0.0, 0.0],  # Red
         [0.0, 1.0, 0.0],  # Green
-        [0.0, 0.0, 1.0]  # Blue
+        [0.0, 0.0, 1.0],  # Blue,
+        [0.89803922, 0.79215686, 0.23921569] # color from picture
     ])
 
-    print(f"codebook: {codebook}")
+    print(f"""codebook: 
+{codebook}
 
-    print(f"codebook.shape: {codebook.shape}")
+""")
 
-    # need to change shape, becouse broadcast will not works otherwise
+    print(f"codebook.shape: {codebook.shape}","\n")
+
+    # need to change shape, because broadcast will not work otherwise
     arr_dist = pixels[:, np.newaxis] - codebook
 
     # np.linalg.norm is used to calculate the distances between each image pixel
@@ -54,28 +59,19 @@ pixels.max: {pixels.max()}
     # find an index of min Euclidean norm, for each pixel-color pair
     labels = np.argmin(euclidean_norms, axis=1)
 
-    distance_for_one_pixel = arr_dist[[165234], :, :]
+    distance_for_one_pixel = arr_dist[[165234], :, :].astype(np.float32)
 
     # print for debug
     print(f"""
-    
 arr_dist.shape: {arr_dist.shape}
         
-arr_dist example: {distance_for_one_pixel}
+arr_dist example(165234 pixel):
 
-""".strip())
+distance_for_one_pixel.shape: {distance_for_one_pixel.shape}
 
-    _, y, _ = distance_for_one_pixel.shape
-
-    print("Provide manual calculation for Euclidean norm for distance above:")
-
-    for n in range(y):
-        vector = distance_for_one_pixel[:, n, :]
-        print(f"""euclidean_norm[{n}]: 
-      {_compute_euclidean_norm(vector=(vector.flatten()))}""")
+{distance_for_one_pixel}""")
 
     print(f"""
-    
 arr_dist.min: {arr_dist.min()}, arr_dist.max: {arr_dist.max()}, arr_dist.mean: {arr_dist.mean()};
         
 euclidean_norms.shape: {euclidean_norms.shape}, euclidean_norms.min: {euclidean_norms.min()}, 
@@ -84,11 +80,22 @@ euclidean_norms.max: {euclidean_norms.max()}, euclidean_norms.mean: {euclidean_n
         
 euclidean_norms example: 
             
-            {euclidean_norms[[165234], :]}
+{euclidean_norms[[165234], :]}
             
 labels: {labels}
-        
-labels: {labels}""".strip())
+
+label for pixel 165234: {labels[165234]}
+""")
+
+    _, y, _ = distance_for_one_pixel.shape
+    print("Provide manual calculation for Euclidean norm for distance above:", "\n")
+    for n in range(y):
+        vector = distance_for_one_pixel[:, [n], :]
+        print(vector)
+        print(f"""euclidean_norm[{n}]: 
+      {_compute_euclidean_norm(vector=(vector.flatten()))}
+
+""")
 
     # Result: now image consist from codebook colors.
     # Color of each pixel of original image maximum close to color of
@@ -98,6 +105,8 @@ labels: {labels}""".strip())
     return quantized_image, codebook, height, width
 
 if __name__ == "__main__":
+
+    np.set_printoptions(suppress=True, precision=4)
 
     path_to_img: str = os.path.join(os.curdir,"..","resources","Collage-of-Python-programming-aspects-syntax-libraries-Python-symbols-logo.jpg")
 
